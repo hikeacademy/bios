4a3045b5c1ecf310bf05648a91c9c9bf457126e5
Gustavo Torres <gustavotorresds@gmail.com>
2018-05-17 18:11:50 -0300

    Construir HTML e CSS.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Meu Primeiro WebApp Incrível', status: 'de boa' });
+});
+
+/* GET Helicopterson bio. */
+router.get('/helicopterson', function(req, res, next) {
+  res.render('bio');
 });
 
 module.exports = router;
